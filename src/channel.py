2147afd116ba060064b0b6f0d03a066486847994
Dmitry Pavlushin <pavlushindv@gmail.com>
2023-12-26 11:30:55 +0500

    git init

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,9 @@
-
+import json
+import os
+from googleapiclient.discovery import build
 
 class Channel:
-    """Класс для ютуб-канала"""
+    ''' Класс для ютуб канала'''
 
-    def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
 
-    def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        pass
+
