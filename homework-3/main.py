333a2e04cebaedc2eaa73a52653a16eeaa2dbf6b
Dmitry Pavlushin <pavlushindv@gmail.com>
2023-12-28 08:40:28 +0500

    add change 2

--- homework-3/main.py
+++ homework-3/main.py
@@ -2,8 +2,8 @@ from src.channel import Channel
 
 if __name__ == '__main__':
     # Создаем два экземпляра класса
-    moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
-    highload = Channel('UCwHL6WHUarjGfUM_586me8w')
+    moscowpython = Channel("UC-OVMPlMA3-YCIeg4z5z23A")
+    highload = Channel("UCwHL6WHUarjGfUM_586me8w")
 
     # Используем различные магические методы
     print(moscowpython)  # 'MoscowPython (https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A)'
--- src/channel.py
+++ src/channel.py
@@ -1,5 +1,6 @@
 import json
 import os
+
 from googleapiclient.discovery import build
 
 
@@ -7,21 +8,10 @@ class Channel:
     """ Класс для ютуб канала """
     API_KEY: str = os.getenv('API_KEY')
     youtube = build('youtube', 'v3', developerKey=API_KEY)
-    #print(os.getenv('API_KEY'))
 
-    def __init__(self, channel_id: str) -> None:
-        """ Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API. """
-        self.channel = None
-        self.__channel_id = channel_id
-        #self.youtube.channels().list(id=channel_id, part='snippets, statistics').execute()
+    def __int__(self, channel_id: str) -> None:
+        self.channel_id = channel_id
+        self.channel = self.youtube.channels().list(id=channel_id, part='snippet, statistics').execute()
+
     def print_info(self) -> None:
-        """ Выводит в консоль информацию о канале """
         print(json.dumps(self.channel))
-    @property
-    def channel_id(self):
-        return self.__channel_id
-
-    @channel_id.setter
-    def channel_id(self, channel_id=None):
-        if channel_id == None:
-            self.__channel_id
