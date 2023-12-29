4488b761e101849f36d7d0e237013624cfbb5873
Dmitry Pavlushin <pavlushindv@gmail.com>
2023-12-29 20:24:12 +0500

    add change (__int__ to __init__)

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,6 +1,8 @@
 from src.channel import Channel
 
-if __name__ == '__main__':
+if __name__ != '__main__':
+    pass
+else:
     moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
     moscowpython.print_info()
 
--- src/channel.py
+++ src/channel.py
@@ -9,9 +9,28 @@ class Channel:
     API_KEY: str = os.getenv('API_KEY')
     youtube = build('youtube', 'v3', developerKey=API_KEY)
 
-    def __int__(self, channel_id: str) -> None:
+    def __init__(self, channel_id: str) -> None:
         self.channel_id = channel_id
         self.channel = self.youtube.channels().list(id=channel_id, part='snippet, statistics').execute()
 
     def print_info(self) -> None:
         print(json.dumps(self.channel))
+
+    @classmethod
+    def get_service(cls):
+        api_key = os.getenv('API_KEY')
+        return build("youtube", "v3", developerKey=api_key)
+
+    def to_json(self, filename):
+        data = {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "link": self.url,
+            "subscribers": self.subscribers,
+            "video_count": self.video_count,
+            "view_count": self.view_count
+        }
+
+        with open(filename, "w") as json_file:
+            json.dump(data, json_file, indent=2)
