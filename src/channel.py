20bd8b50aad479ff0ce692fcc95e9c21fd5561dd
Dmitry Pavlushin <pavlushindv@gmail.com>
2023-12-31 11:06:14 +0500

    Well done

--- src/channel.py
+++ src/channel.py
@@ -3,34 +3,103 @@ import os
 
 from googleapiclient.discovery import build
 
-
 class Channel:
-    """ Класс для ютуб канала """
+
     API_KEY: str = os.getenv('API_KEY')
     youtube = build('youtube', 'v3', developerKey=API_KEY)
 
     def __init__(self, channel_id: str) -> None:
+        youtube_channel = 'https://www.youtube.com/channel/'
         self.channel_id = channel_id
-        self.channel = self.youtube.channels().list(id=channel_id, part='snippet, statistics').execute()
+        self.channel_title: str = self.snippet['title']
+        self.channel_description: str = self.snippet['description']
+        self.channel_url: str = youtube_channel + self.channel_id
+        self.channel_subs: int = self.to_int(self.statistics['subscriberCount'])
+        self.channel_total_video: int = self.to_int(self.statistics['videoCount'])
+        self.channel_total_views = self.to_int(self.statistics['viewCount'])
+
 
     def print_info(self) -> None:
-        print(json.dumps(self.channel))
+
+        self.print_dict(self.channel_info)
+
+    @property
+    def channel_info(self) -> dict:
+
+        result: dict = self.youtube.channels().list(part='snippet,statistics', id=self.channel_id).execute()
+        return result['items'][0]
+
+    @staticmethod
+    def print_dict(dict_to_print: dict) -> None:
+
+        print(json.dumps(dict_to_print, indent=2))
 
     @classmethod
-    def get_service(cls):
-        api_key = os.getenv('API_KEY')
-        return build("youtube", "v3", developerKey=api_key)
+    def get_service(cls) -> object:
 
-    def to_json(self, filename):
+        return cls.youtube
+
+
+    @property
+    def title(self) -> str:
+
+        return self.channel_title
+
+    @property
+    def description(self) -> str:
+
+        return self.channel_description
+
+    @property
+    def url(self) -> str:
+
+        return self.channel_url
+
+    @property
+    def subscribers(self) -> int:
+
+        return self.channel_subs
+
+    @property
+    def video_count(self) -> int:
+
+        return self.channel_total_video
+
+    @property
+    def views(self) -> int:
+
+        return self.channel_total_views
+
+    def to_json(self, file_title: str) -> None:
+
         data = {
-            "id": self.id,
-            "title": self.title,
-            "description": self.description,
-            "link": self.url,
-            "subscribers": self.subscribers,
-            "video_count": self.video_count,
-            "view_count": self.view_count
+            "id": self.channel_id,
+            "title": self.channel_title,
+            "description": self.channel_description,
+            "url": self.channel_url,
+            "subscriberCount": self.channel_subs,
+            "videoCount": self.channel_total_video,
+            "viewCount": self.channel_total_views
         }
 
-        with open(filename, "w") as json_file:
-            json.dump(data, json_file, indent=2)
+        with open(file_title, 'w', encoding='utf-8') as filename: json.dump(data, filename, indent=2)
+
+    @staticmethod
+    def to_int(string_num: str) -> int:
+
+        return int(float(string_num))
+
+    @property
+    def snippet(self) -> dict:
+
+        return self.channel_info['snippet']
+
+    @property
+    def statistics(self) -> dict:
+
+        return self.channel_info['statistics']
+
+    def __repr__(self) -> str:
+
+        return f"{self.channel_id}"
+
