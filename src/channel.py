fe4901a99c1c5ae671fa2263740a9e01a2a64e44
Dmitry Pavlushin <pavlushindv@gmail.com>
2024-01-10 20:44:05 +0300

    14.1

--- src/channel.py
+++ src/channel.py
@@ -3,8 +3,8 @@ import os
 
 from googleapiclient.discovery import build
 
-class Channel:
 
+class Channel:
     API_KEY: str = os.getenv('API_KEY')
     youtube = build('youtube', 'v3', developerKey=API_KEY)
 
@@ -18,60 +18,47 @@ class Channel:
         self.channel_total_video: int = self.to_int(self.statistics['videoCount'])
         self.channel_total_views = self.to_int(self.statistics['viewCount'])
 
-
     def print_info(self) -> None:
-
         self.print_dict(self.channel_info)
 
     @property
     def channel_info(self) -> dict:
-
         result: dict = self.youtube.channels().list(part='snippet,statistics', id=self.channel_id).execute()
         return result['items'][0]
 
     @staticmethod
     def print_dict(dict_to_print: dict) -> None:
-
         print(json.dumps(dict_to_print, indent=2))
 
     @classmethod
     def get_service(cls) -> object:
-
         return cls.youtube
-
 
     @property
     def title(self) -> str:
-
         return self.channel_title
 
     @property
     def description(self) -> str:
-
         return self.channel_description
 
     @property
     def url(self) -> str:
-
         return self.channel_url
 
     @property
     def subscribers(self) -> int:
-
         return self.channel_subs
 
     @property
     def video_count(self) -> int:
-
         return self.channel_total_video
 
     @property
     def views(self) -> int:
-
         return self.channel_total_views
 
     def to_json(self, file_title: str) -> None:
-
         data = {
             "id": self.channel_id,
             "title": self.channel_title,
@@ -84,22 +71,46 @@ class Channel:
 
         with open(file_title, 'w', encoding='utf-8') as filename: json.dump(data, filename, indent=2)
 
+    def __str__(self):
+        return f"{self.title} ({self.url})"
+
     @staticmethod
     def to_int(string_num: str) -> int:
-
         return int(float(string_num))
 
     @property
     def snippet(self) -> dict:
-
         return self.channel_info['snippet']
 
     @property
     def statistics(self) -> dict:
-
         return self.channel_info['statistics']
 
     def __repr__(self) -> str:
-
         return f"{self.channel_id}"
 
+    @classmethod
+    def compare(cls, other):
+        if not isinstance(other, (int, Channel)):
+            raise TypeError("Операнд справа должен быть числом или объектом класса")
+        return other if isinstance(other, int) else int(other.channel_subs)
+
+    def __add__(self, other):
+        subscription = self.compare(other)
+        return int(self.channel_subs) + int(subscription)
+
+    def __deduction__(self, other):
+        subscription = self.compare(other)
+        return int(self.channel_subs) - int(subscription)
+
+    def __equals__(self, other):
+        subscription = self.compare(other)
+        return int(self.channel_subs) == int(subscription)
+
+    def __more__(self, other):
+        subscription = self.compare(other)
+        return int(self.channel_subs) > int(subscription)
+
+    def __less__(self, other):
+        subscription = self.compare(other)
+        return int(self.channel_subs) < int(subscription)
